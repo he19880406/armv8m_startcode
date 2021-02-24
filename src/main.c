5ff8757667e04bce28688e58ab1cbbf66eec2bd0
hepengfei01 <hepengfei01@inspur.com>
2021-02-23 17:16:45 -0800

    Change link script.

--- src/main.c
+++ src/main.c
@@ -1,15 +1,4 @@
 
-#define REG32(addr) ((volatile unsigned int *)(addr))
-
-#define readl(a) (*REG32(a))
-#define writel(v, a) (*REG32(a) = (v))
-
-#define HFSR 0xE000ED2C
-
-void __aeabi_unwind_cpp_pr0()
-{
-}
-
 void SystemInit(void)
 {
 
